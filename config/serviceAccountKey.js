b74dcbe350a9ffe6dd1412c488e8b6c8dc18d8bb
jaymalave <jaym.github@gmail.com>
2022-04-19 22:41:47 +0530

    feat: add service account

---
+++ config/serviceAccountKey.js
@@ -0,0 +1,14 @@
+export default serviceAccountKey = {
+  type: "service_account",
+  project_id: "quizapp-d0e54",
+  private_key_id: "a08978c1e69e5d8fdf1a03e501084500255129c0",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDf7yrYO27Ylvje\n5FJGwfjS6PzM4Lp6oMYw9MAJM0ya4hmHKkg41mgBnpcFEEKYP2575F7KAsTwoMMJ\nnbZgXQUedMLrRkvd0xOc5nYwK5394z9/l1rdsK821v/UAOvye7rkQYYB6iwGFjFf\nj4MbtEOP8bmVPLz8FqYdsr2YkNRuCDNsX6aRG8+Gzwe5sQGLjov7tIfNPrXG+qye\nT+Qx4V6DIZa94k4u1ym0RKjjFKevqgsxfu1B7cAV34XNl3OMA3W5WpYi4d+8HjJL\nxhk43COyWt6CRCqGTQrZrjU4DEy2AE3ivSw54p8+E71fcm7RE1oyqhHoXHr/GlL0\nUGELSVDvAgMBAAECggEABPZNVBP3vJx6CYZfBFyYb5XH3vUD3pw2Ty/ZqYUyJ11Z\nBMygnKAI+trfrH1FwA7NG0d6pq34leKoTC9c6+DVk/tp7k/VIMLH9LM1wsvxE6Ni\nl7bBVAmYsTzYLRHMmnJGjbC6c68TmjbtA2Da8438JbkT9wUQa+Lx7ZxR5Pyu0UP9\nwLxOr6B04DLU3X0Av7ZWkDtJDbG4BVgrc9To0RVHHFPZ9kOQR0L3vvHriTOto39c\n/kgVGDpwkwEEv28fIc/3x59ptV7dbNIc7sTWZ8cTx7VAqlv/6nyS1S/zFoI65wue\nSXsg4uemkJXrm3SX0Y55Y0MZlZ0Ky8viPJXQEPcsoQKBgQD1HEvXb16OYRMYRnie\nlZerQOnu2HG99qJ0OKm/CX2XCWA9XM08AH0Kmb9uqYLKC+U5sdQ6LNQJpn1KZCe0\nFmNnwlKQVZ5y6JAtcKFmaUBxfvn55AAWNqX5Tq/NuEkTYpgLU8tRxrJ9UCAz+nu7\nX6NiB7sbKxInkd2ooDlBIhRRtwKBgQDp4gcj9FTYrFjpopq/wTOgdnXBfCqQo4kW\nCp+5UF6S+V7Gu1gAaE4nEjw9jYA0/FISCosyQ4ZlSXUAOA/APCjNrL5gY9MDWQ/O\nP8gou5pxMM6NfGuOmp+CrfSZ/dOBpo8862FQ58CozU/947HEBsFIwVpOow0QfN1t\npOWcnssaiQKBgQDMl0zayxQDBhY5KgFeZYEOqgVZnwL5zBb3qaBOXC6fVm0lawPI\nNKQqqAC47vbDjUBNcxixYl+umnB0GrtlP1bD8diWr1Ctm19U/nFKGUp4McOYCO7l\nm8HYIgzuSwd8DczZj5zjalXr8Wif2lFTKlYm6ksMN1xU+RUqvmpr6nVJawKBgQC7\nmpOxsnYwiyaS2dyZfccEqqDQP+IAvTzkEkeGpUIHUMJGVNZFFWXDD2yEzzEMz2yh\njhWTjviJ3H9eWpm/0EHGoQ7gUJJdeB4miaDrk3hR3R2B/lBkRgE3GIRA1YrHz6vL\n6N7Zv9wZSv8kH3VjUSvPSVFADKgZFp+jKIcJt5IhSQKBgQDBn4dnnuQEKN48W3hh\nMwn1LO/TdVbj+/UfWprmFr/JyHI7Mjddrh7OS81+qKgErwLJgTHn+t6wr4bmVotL\ndmCLv8E9+Zb0LqmoZYKOay4h09Fku3zajTJCr7gHx7bhLBg/K//84e2khwUgKfGx\n6bich9VKPr2jHEWHFRcb9cOn8Q==\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-sj0vy@quizapp-d0e54.iam.gserviceaccount.com",
+  client_id: "104102584535506329580",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sj0vy%40quizapp-d0e54.iam.gserviceaccount.com",
+};
